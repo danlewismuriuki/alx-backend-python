87f4092f909ab9cc09ea81f10d95fb8ad8ae3353
Dan <danlewismuriuki@gmail.com>
2024-08-01 15:35:13 +0300

    Remove trailing whitespace in 7-to_kv.py to conform with pycodestyle
    - Cleared trailing whitespace from lines to ensure compliance with PEP 8
    - Improved code formatting for better readability and style consistency

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,8 +2,8 @@
 """
     function to_kv that takes a string k and an int OR float v as arguments
     returns a tuple. The first element of the tuple is the string k.
-    The second element is the square of the int/float v and 
-        should be annotated as a float.
+    The second element is the square of the int/float v and
+    should be annotated as a float.
 """
 from typing import List, Union, Tuple
 
