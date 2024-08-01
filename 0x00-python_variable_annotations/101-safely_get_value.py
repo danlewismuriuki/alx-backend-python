936d5598193a39e3737eef1e4751a8aea0c7d836
Dan <danlewismuriuki@gmail.com>
2024-08-01 23:33:13 +0300

    Enhance type annotations in `safely_get_value` function
    - Improved type annotations to include generic type variable `T`.
    - Updated function signature for better type safety and clarity.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,27 +1,15 @@
 #!/usr/bin/env python3
-"""
-More involved type annotations
-Add type annotations to function safely_get_value
-"""
-from typing import Mapping, Union, TypeVar, Any
+""" More involved type annotations  """
+from typing import Mapping, Any, Sequence, Union, TypeVar
 
-# Define a TypeVar to represent the type of the default value
+
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default:
-                     Union[T, None] = None) -> Union[T, None]:
-    """
-    Safely retrieves a value from a dictionary or returns a default value.
-
-    Args:
-        dct (Mapping[Any, T]): The dictionary from which to retrieve the value.
-        key (Any): The key to look up in the dictionary.
-        default (Union[T, None]): default value to return if key not found.
-
-    Returns:
-        Union[T, None]: The value associated with the key or the default value.
-    """
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """ Safely get value """
     if key in dct:
         return dct[key]
     else:
