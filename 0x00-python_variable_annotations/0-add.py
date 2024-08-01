02d964ae913e3fec4f44a4c608e62f700091da09
Dan <danlewismuriuki@gmail.com>
2024-08-01 13:17:45 +0300

    Add type-annotated `add` function and documentation
    - Implemented `add` function with type annotations specifying float arguments and return type.
    - Added documentation for the function and its usage.
    - Verified function and annotations with a test script.

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,2 +1,13 @@
-def add(a : float, b : float) -> float:
+#!/usr/bin/env python3
+
+"""
+function add that takes a float a and a float b as arguments
+returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    returns their sum as a float.
+    """
     return a + b
