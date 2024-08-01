5b8a18ef2b9546d2b6ef1fbe4ed89026eb73eae7
Dan <danlewismuriuki@gmail.com>
2024-08-01 13:40:38 +0300

    Implement type-annotated floor function
    - Defined a type-annotated function `floor` that takes a float and returns the floored integer value.
    - Used `math.floor` for computing the floor of the float.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import math
+"""
+function floor which takes a float n as argument
+returns the floor of the float.
+"""
+
+
+def floor(n: float) -> float:
+    """
+    returns the floor of the float.
+    """
+    return math.floor(n)
