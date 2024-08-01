f3e3084708ff12768e87671cdb33ff844692fec6
Dan <danlewismuriuki@gmail.com>
2024-08-01 15:33:11 +0300

    Fix line length issues in 7-to_kv.py
    - Adjusted lines to meet the 79-character limit as per PEP 8 style guide
    - Reformatted code to improve readability and maintain coding standards

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,7 +2,8 @@
 """
     function to_kv that takes a string k and an int OR float v as arguments
     returns a tuple. The first element of the tuple is the string k.
-    The second element is the square of the int/float v and should be annotated as a float.
+    The second element is the square of the int/float v and 
+        should be annotated as a float.
 """
 from typing import List, Union, Tuple
 
@@ -13,7 +14,8 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
             k: A string (str).
             v: Can be either an integer (int) or a float (float).
         Return Type:
-            Tuple[str, float]: A tuple where the first element is a string and the second element is a float.
+            Tuple[str, float]: A tuple where the first element is a string
+                the second element is a float.
     """
 
     return (k, (v * v))
