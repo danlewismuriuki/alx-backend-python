146ddb565c72f753934c268037877c1b91dd86eb
Dan <danlewismuriuki@gmail.com>
2024-08-05 18:58:32 +0300

    Add comments and docstring to wait_random function
    - Added shebang line explanation
    - Documented imports for clarity
    - Added docstring to explain the function's purpose, parameters, and return value
    - Included comments on generating delay, pausing execution, and returning the delay

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+    asynchronous coroutine that takes in an integer argument (max_delay,
+    with a default value of 10) named wait_random
+    that waits for a random delay between 0 and max_delay (included
+    and float value) seconds and eventually returns it.
+"""
+
+import random
+import asyncio
+
+"""
+    asynchronous coroutine that takes in an integer argument (max_delay,
+    parameters:
+    (max_delay)(int): The maximum delay in seconds. Default is 10.
+
+    Returns:
+    float: The actual delay time in seconds.
+"""
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
