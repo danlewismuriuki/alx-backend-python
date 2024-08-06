d2db6b139dc25a0acf0f5c439648c9c2e2f0ac3a
Dan <danlewismuriuki@gmail.com>
2024-08-06 19:39:02 +0300

    Update task_wait_random function to use asyncio.create_task
    - Added asyncio. prefix to create_task to clarify its module origin.
    - Ensured that the return type is asyncio.Task, as the function creates and returns an asyncio Task object.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,24 +1,13 @@
 #!/usr/bin/env python3
-
-"""
-    function task_wait_random that takes an integer max_delay
-    returns asyncio.Task.
-"""
-
+'''Task 3's module.
+'''
 import asyncio
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay) -> asyncio.Task:
-    """
-    Create and return an asyncio.Task to run the wait_random coroutine.
-
-    Args:
-    max_delay (int): The maximum delay in seconds for the wait_random
-    coroutine.
-
-    Returns;
-    asyncio.Task: A Task object that represents the wait_random coroutine.
-    """
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    '''Creates an asynchronous task for wait_random.
+    '''
     return asyncio.create_task(wait_random(max_delay))
