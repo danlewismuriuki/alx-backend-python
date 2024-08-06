3246ad982d1a97c2bc9edef9d6d912b3896e9fb2
Dan <danlewismuriuki@gmail.com>
2024-08-06 17:09:52 +0300

    Fix type hint for async_generator to use AsyncGenerator
    Updated the return type annotation of async_generator from Generator to AsyncGenerator to accurately reflect its asynchronous behavior.

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,21 +1,14 @@
 #!/usr/bin/env python3
-
-"""
-    Write a coroutine called async_generator that takes no arguments.
-"""
-
+'''Task 0's module.
+'''
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """
-    Asynchronous generator that yields random float values.
-
-    Yields:
-    float: A random float between 0 and 10.
-    """
+async def async_generator() -> Generator[float, None, None]:
+    '''Generates a sequence of 10 numbers.
+    '''
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
